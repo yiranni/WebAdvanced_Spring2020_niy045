c9bc268aface52e76dda9229dcf308f2e399578e
Yiran Ni <yiran@yirans-mbp.myfiosgateway.com>
2020-02-25 17:56:25 -0500

    add jQuery functions

--- week3/script.js
+++ week3/script.js
@@ -1,1 +1,6 @@
-console.log("JS is connected");
+$('.wrapper').find('a[href="#"]').on('click', function (e) {
+    e.preventDefault();
+    this.expand = !this.expand;
+    $(this).text(this.expand ? "Click to collapse" : "Click to read more");
+    $(this).closest('.wrapper').find('.small, .big').toggleClass('small big');
+});
